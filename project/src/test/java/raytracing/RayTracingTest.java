d20de0d587afbcb13162d79602941937b6a05563
Lolo <lolo@Lolo-pc.home>
2017-05-27 23:22:26 +0200

    Reduce image size

--- project/src/test/java/raytracing/RayTracingTest.java
+++ project/src/test/java/raytracing/RayTracingTest.java
@@ -27,23 +27,25 @@ import renderers.destination.BmpRenderDestination;
 public class RayTracingTest {
 
 	private void test(URL url) throws IOException {
-		int width = 500;
-		int height = 300;
-		
+		int width = 300;
+		int height = 200;
+
 		final Object3D obj = new ObjFileLoader().load(url);
-		double cameraDistance = Math.max(Math.max(obj.getBoundingBox().getWidth(), obj.getBoundingBox().getHeight()), obj.getBoundingBox().getDepth());
-		final CenteredCamera c = new CenteredCamera(obj.getBoundingBox().center().translate(new Vector(0, 0, -20)), cameraDistance, new Vector(1, 1, -1), new Vector(0, 0, 1).normalize(), 1, width / (double)height, 1, new Dimension(width, height));
-		Scene sce=new Scene();
+		double cameraDistance = Math.max(Math.max(obj.getBoundingBox().getWidth(), obj.getBoundingBox().getHeight()),
+				obj.getBoundingBox().getDepth());
+		final CenteredCamera c = new CenteredCamera(obj.getBoundingBox().center().translate(new Vector(0, 0, -20)),
+				cameraDistance, new Vector(1, 1, -1), new Vector(0, 0, 1).normalize(), 1, width / (double) height, 1,
+				new Dimension(width, height));
+		Scene sce = new Scene();
 		sce.addObject(obj);
-        AbstractRayTracingRenderer renderer = new RayTracingRenderer();
-        renderer.prepare(sce);
-        
-        
-        final BmpRenderDestination bmp = new BmpRenderDestination(width, height);
-		
-        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 10);
-        server.setExecutor(Executors.newCachedThreadPool());
-        server.createContext("/index", new HttpHandler() {
+		AbstractRayTracingRenderer renderer = new RayTracingRenderer();
+		renderer.prepare(sce);
+
+		final BmpRenderDestination bmp = new BmpRenderDestination(width, height);
+
+		HttpServer server = HttpServer.create(new InetSocketAddress(8000), 10);
+		server.setExecutor(Executors.newCachedThreadPool());
+		server.createContext("/index", new HttpHandler() {
 			@Override
 			public void handle(HttpExchange ex) throws IOException {
 				String answer = "<html><body style=\"background-image: url(/image); background-position: center; background-size: contain; background-repeat: no-repeat;\"></body></html>";
@@ -52,43 +54,43 @@ public class RayTracingTest {
 				os.write(answer.getBytes());
 				os.close();
 			}
-        });
-        server.createContext("/image", new HttpHandler() {
+		});
+		server.createContext("/image", new HttpHandler() {
 			@Override
 			public void handle(HttpExchange ex) throws IOException {
 				ex.getResponseHeaders().set("Content-Disposition", "inline; filename=\"render.bmp\"");
 				ex.getResponseHeaders().set("Content-Type", "image/bmp");
-	            ex.sendResponseHeaders(200, 0);
-	            OutputStream os = ex.getResponseBody();
-	            
+				ex.sendResponseHeaders(200, 0);
+				OutputStream os = ex.getResponseBody();
+
 				InputStream stream = bmp.getInputStream();
 				int data;
 				try {
-					while((data = stream.read()) >= 0) {
+					while ((data = stream.read()) >= 0) {
 						os.write(data);
 					}
-				} catch(Throwable t) {
+				} catch (Throwable t) {
 				}
-	            os.close();
+				os.close();
 			}
 		});
-        server.start();
-        
-        System.out.println("TECHIO> open --port 8000 /index");
+		server.start();
 
-        renderer.render(c, bmp);
+		System.out.println("TECHIO> open --port 8000 /index");
+
+		renderer.render(c, bmp);
 	}
-	
-//	@Test
-//	public void testMiniCooper() throws IOException {
-//		test(RayTracingTest.class.getResource("/minicooper.obj"));
-//	}
-	
+
+	// @Test
+	// public void testMiniCooper() throws IOException {
+	// test(RayTracingTest.class.getResource("/minicooper.obj"));
+	// }
+
 	@Test
 	public void testTeapot() throws IOException {
 		test(RayTracingTest.class.getResource("/teapot.obj"));
 	}
-	
+
 	@After
 	public void waitViewer() throws InterruptedException {
 		Thread.sleep(1000000);
