e966304c9619bd70817e5ec7f0f89f6c95765b7f
Lolo <lolo@Lolo-pc.home>
2017-05-28 11:07:31 +0200

    Add debug logs

--- project/src/main/java/renderers/AbstractRayTracingRenderer.java
+++ project/src/main/java/renderers/AbstractRayTracingRenderer.java
@@ -56,9 +56,9 @@ public abstract class AbstractRayTracingRenderer implements Renderer {
 			int counter = 0;
 			while ((result = results.poll()) != null) {
 				render.writeLine(result.get());
-				if (counter % 50 == 0) {
+//				if (counter % 2 == 0) {
 					System.out.println(counter + " line computed");
-				}
+//				}
 				counter ++;
 			}
 		} catch (IOException | ExecutionException | InterruptedException e) {
