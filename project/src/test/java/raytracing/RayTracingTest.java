62f136bd3c271b980b0959f7c41425ade62b6d24
Lolo <lolo@Lolo-pc.home>
2017-05-30 00:26:32 +0200

    Use jetty for web server

--- project/src/test/java/raytracing/RayTracingTest.java
+++ project/src/test/java/raytracing/RayTracingTest.java
@@ -13,7 +13,13 @@ import java.nio.file.Paths;
 import java.util.concurrent.Executors;
 
 import javax.imageio.ImageIO;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.AbstractHandler;
 import org.junit.After;
 import org.junit.Test;
 
@@ -33,7 +39,7 @@ import renderers.destination.BmpRenderDestination;
 public class RayTracingTest {
 
 	@SuppressWarnings("restriction")
-	private void test(Dimension resolution, Vector cameraDirection, Vector cameraNormal, Vector translate, double distance, URL url) throws IOException {
+	private void test(Dimension resolution, Vector cameraDirection, Vector cameraNormal, Vector translate, double distance, URL url) throws Exception {
 		int width = resolution.width;
 		int height = resolution.height;
 
@@ -49,84 +55,131 @@ public class RayTracingTest {
 		renderer.prepare(sce);
 
 		final BmpRenderDestination bmp = new BmpRenderDestination(width, height);
-		HttpServer server = HttpServer.create(new InetSocketAddress(8000), 10);
-		server.setExecutor(Executors.newCachedThreadPool());
-		server.createContext("/index", new HttpHandler() {
+		
+        Server server = new Server(8000);
+        server.setHandler(new AbstractHandler() {
 			@Override
-			public void handle(HttpExchange ex) throws IOException {
-				try {
-					String answer = new String(Files.readAllBytes(Paths.get(RayTracingTest.class.getResource("/index.html").toURI())));
-
-					byte[] bytes = answer.getBytes();
-					ex.sendResponseHeaders(200, 0);
-					OutputStream os = ex.getResponseBody();
-					os.write(bytes);
-					os.flush();
-					ex.close();
-				} catch (URISyntaxException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-		});
-		server.createContext("/image", new HttpHandler() {
-			@Override
-			public void handle(HttpExchange ex) throws IOException {
-				ex.getResponseHeaders().set("Content-Disposition", "inline; filename=\"render.bmp\"");
-				ex.getResponseHeaders().set("Content-Type", "image/bmp");
-				ex.sendResponseHeaders(200, 0);
-				OutputStream os = ex.getResponseBody();
-
-				
-				int data;
-				try (InputStream stream = bmp.getInputStream()) {
-					while ((data = stream.read()) >= 0) {
-						os.write(data);
+			public void handle(String target,
+                    Request baseRequest,
+                    HttpServletRequest request,
+                    HttpServletResponse response)
+					throws IOException, ServletException {
+				switch(target) {
+				case "/index":
+					System.out.println("test");
+					try {
+						String answer = new String(Files.readAllBytes(Paths.get(RayTracingTest.class.getResource("/index.html").toURI())));
+	
+				        response.setContentType("text/html;charset=utf-8");
+				        response.setStatus(HttpServletResponse.SC_OK);
+				        baseRequest.setHandled(true);
+				        response.getWriter().println(answer);
+					} catch (URISyntaxException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
 					}
-				} catch (Throwable t) {
+					break;
+				case "/image":
+			        response.setContentType("image/bmp");
+			        response.setStatus(HttpServletResponse.SC_OK);
+			        response.setHeader("Content-Disposition", "inline; filename=\"render.bmp\"");
+	
+					int data;
+					try (InputStream stream = bmp.getInputStream()) {
+						while ((data = stream.read()) >= 0) {
+							response.getOutputStream().write(data);
+						}
+					} catch (Throwable t) {
+					}
+					break;
+				case "/imagejpg":
+			        response.setContentType("image/bmp");
+			        response.setStatus(HttpServletResponse.SC_OK);
+			        response.setHeader("Content-Disposition", "inline; filename=\"render.jpg\"");
+	
+					try (InputStream stream = bmp.getFilledInputStream()) {
+						BufferedImage image = ImageIO.read(stream);
+						ImageIO.write(image, "jpg", response.getOutputStream());
+					}
+					break;
 				}
-				os.close();
-			}
-		});
-		server.createContext("/imagejpg", new HttpHandler() {
-			@Override
-			public void handle(HttpExchange ex) throws IOException {
-				ex.getResponseHeaders().set("Content-Disposition", "inline; filename=\"render.jpg\"");
-				ex.getResponseHeaders().set("Content-Type", "image/jpg");
-				ex.sendResponseHeaders(200, 0);
-				OutputStream os = ex.getResponseBody();
 
-				try (InputStream stream = bmp.getFilledInputStream()) {
-					BufferedImage image = ImageIO.read(stream);
-					ImageIO.write(image, "jpg", os);
-				}
-				os.close();
 			}
-		});
-		server.start();
+        });
+ 
+        server.start();
+        
+//		HttpServer server = HttpServer.create(new InetSocketAddress(8000), 10);
+//		server.setExecutor(Executors.newCachedThreadPool());
+//		server.createContext("/index", new HttpHandler() {
+//			@Override
+//			public void handle(HttpExchange ex) throws IOException {
+//				try {
+//					String answer = new String(Files.readAllBytes(Paths.get(RayTracingTest.class.getResource("/index.html").toURI())));
+//
+//					byte[] bytes = answer.getBytes();
+//					ex.sendResponseHeaders(200, 0);
+//					OutputStream os = ex.getResponseBody();
+//					os.write(bytes);
+//					os.flush();
+//					ex.close();
+//				} catch (URISyntaxException e) {
+//					// TODO Auto-generated catch block
+//					e.printStackTrace();
+//				}
+//			}
+//		});
+//		server.createContext("/image", new HttpHandler() {
+//			@Override
+//			public void handle(HttpExchange ex) throws IOException {
+//				ex.getResponseHeaders().set("Content-Disposition", "inline; filename=\"render.bmp\"");
+//				ex.getResponseHeaders().set("Content-Type", "image/bmp");
+//				ex.sendResponseHeaders(200, 0);
+//				OutputStream os = ex.getResponseBody();
+//
+//				
+//				int data;
+//				try (InputStream stream = bmp.getInputStream()) {
+//					while ((data = stream.read()) >= 0) {
+//						os.write(data);
+//					}
+//				} catch (Throwable t) {
+//				}
+//				os.close();
+//			}
+//		});
+//		server.createContext("/imagejpg", new HttpHandler() {
+//			@Override
+//			public void handle(HttpExchange ex) throws IOException {
+//				ex.getResponseHeaders().set("Content-Disposition", "inline; filename=\"render.jpg\"");
+//				ex.getResponseHeaders().set("Content-Type", "image/jpg");
+//				ex.sendResponseHeaders(200, 0);
+//				OutputStream os = ex.getResponseBody();
+//
+//				try (InputStream stream = bmp.getFilledInputStream()) {
+//					BufferedImage image = ImageIO.read(stream);
+//					ImageIO.write(image, "jpg", os);
+//				}
+//				os.close();
+//			}
+//		});
+//		server.start();
 		System.out.println("TECHIO> open --port 8000 /index");
 		System.out.println("TECHIO> success");
 		renderer.render(c, bmp);
 		
-		System.out.println("gc");
-		System.gc();
-		System.out.println("end gc");
+		server.join();
 	}
 
 	@Test
-	public void testMiniCooper() throws IOException {
+	public void testMiniCooper() throws Exception {
 		test(new Dimension(700, 400), new Vector(1, 1, -1).normalize(), new Vector(0, 0, 1).normalize(), new Vector(20, -10, -25), 1,
 				RayTracingTest.class.getResource("/minicooper.obj"));
 	}
 
-	@Test
-	public void testTeapot() throws IOException {
-		test(new Dimension(1280, 720), new Vector(-1, -1, -1).normalize(), new Vector(0, 1, 0).normalize(), new Vector(40, 20, 15), 0.5,
-				RayTracingTest.class.getResource("/teapot.obj"));
-	}
-
-	@After
-	public void waitViewer() throws InterruptedException {
-		Thread.sleep(1000000);
-	}
+//	@Test
+//	public void testTeapot() throws Exception {
+//		test(new Dimension(1280, 720), new Vector(-1, -1, -1).normalize(), new Vector(0, 1, 0).normalize(), new Vector(40, 20, 15), 0.5,
+//				RayTracingTest.class.getResource("/teapot.obj"));
+//	}
 }
